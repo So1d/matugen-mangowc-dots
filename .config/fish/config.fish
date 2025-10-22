b3fee485aa17e0fae5d2d15bd494aacd755a9a43
Luis Dutra <machadodt@tutamail.com>
2025-10-21 22:53:32 -0300

    k

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+alias zp="sudo zypper"
+alias call="distrobox enter"
+alias sonofman="flatpak run org.hlwd.sonofman"
+alias legacylauncher="java -jar /home/machado/Jogos/MinecraftLL/LegacyLauncher_legacy.jar"
+alias wlm="wl-mirror eDP-1"
+alias wlm-c="wl-mirror -c eDP-1"
+alias fish-alias="nano .config/fish/config.fish"
+alias zen='flatpak run app.zen_browser.zen'
+end
